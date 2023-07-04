ca303b73db315d6e834e537b93d0dae629360645
syed zunair gillani <gillanix007@gmail.com>
2023-07-04 19:58:20 +0500

    update

--- next-sitemap.js
+++ next-sitemap.js
@@ -10,36 +10,44 @@ const query = `
     }
   }
 `;
-var jobsquery = `
+
+const jobsQuery = `
   query AllJobs {
-     jobs {
-       nodes {
-         slug
-         date
-       }
-     }
-   }
+    jobs {
+      nodes {
+        slug
+        date
+      }
+    }
+  }
 `;
 
-SITE_URI="https://truejob.online"
+const SITE_URI = 'https://truejob.online';
 
-async function generateSitemap() {
-     const response = await fetch('https://truejob.designercrunch.net/graphql', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ query }),
-        });
-        const { data } = await response.json();
-  const jobresponse = await fetch('https://truejob.designercrunch.net/graphql', {
+async function fetchData() {
+  const response = await fetch('https://truejob.designercrunch.net/graphql', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ jobsquery }),
+    body: JSON.stringify({ query }),
   });
-  const jobsdata = await jobresponse.json();
+  const { data } = await response.json();
+
+  const jobsResponse = await fetch('https://truejob.designercrunch.net/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query: jobsQuery }),
+  });
+  const jobsData = await jobsResponse.json();
+
+  return { data, jobsData };
+}
+
+async function generateSitemap() {
+  const { data, jobsData } = await fetchData();
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -49,29 +57,37 @@ async function generateSitemap() {
         <priority>1.00</priority>
       </url>
       <url>
-          <loc>${SITE_URI}/contact-us</loc>
-          <lastmod>2023-05-11T19:00:11+00:00</lastmod>
-          <priority>1.00</priority>
-     </url>
-     <url>
-          <loc>${SITE_URI}/about-us</loc>
-          <lastmod>2023-05-11T19:00:11+00:00</lastmod>
-          <priority>1.00</priority>
-     </url>
-     ${data?.posts?.nodes.map((item) => `
+        <loc>${SITE_URI}/contact-us</loc>
+        <lastmod>2023-05-11T19:00:11+00:00</lastmod>
+        <priority>1.00</priority>
+      </url>
+      <url>
+        <loc>${SITE_URI}/about-us</loc>
+        <lastmod>2023-05-11T19:00:11+00:00</lastmod>
+        <priority>1.00</priority>
+      </url>
+      ${data?.posts?.nodes
+        .map(
+          (item) => `
         <url>
           <loc>${SITE_URI}/post/${item.slug}</loc>
           <lastmod>${item.date}</lastmod>
           <priority>0.80</priority>
-        </url>`).join('')
-     }
-     ${jobsdata?.data?.jobs?.nodes.map((item) => `
+        </url>
+      `
+        )
+        .join('')}
+      ${jobsData?.data?.jobs?.nodes
+        .map(
+          (item) => `
         <url>
           <loc>${SITE_URI}/jobs/${item.slug}</loc>
           <lastmod>${item.date}</lastmod>
           <priority>0.80</priority>
-        </url>`).join('')
-     }
+        </url>
+      `
+        )
+        .join('')}
     </urlset>
   `;
 
